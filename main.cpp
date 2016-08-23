68a52c94e83f9b828f82d8a1a0cae512e7edb08d
winer632 <winer632@163.com>
2016-08-23 20:39:39 +0800

    test

--- main.cpp
+++ main.cpp
@@ -44,8 +44,9 @@ int main(int argc, char *argv[])
 	db.init("127.0.0.1:11008", "root", "fdjkd&wi278^@6DGHfyTF");
 	db.connect();
 	db.switchDb("mihao");
-	db.prepare("INSERT INTO active_user(create_time, mdn, imsi, esn) VALUES (?, ?, ?, ?)");
 
+#if 0	
+	db.prepare("INSERT INTO active_user(create_time, mdn, imsi, esn) VALUES (?, ?, ?, ?)");
 	string now=currentDateTime();
 	db.setString(1,now);
 	db.setString(2,"13301602770");
@@ -64,12 +65,21 @@ int main(int argc, char *argv[])
 	db.setString(3, "6301743459");
 	db.setString(4, "8094016C");
 	db.execute();
+#endif
 
+	char mdn[]="13301605371";
+	char sql[100];
+	sprintf(sql,"delete from active_user where mdn=%s",mdn);
+	string query(1,sql);
+	db.execute(query);
+	
 
+#if 0
 	//db.execute("select * from active_user where mdn=13301602770");
 	db.execute("select * from active_user");
 	db.closeCon();
-	
+#endif
+
 	while(db.fetch()){
 		string create_time=db.getString("create_time");
 		printf("create_time=%s\n", create_time.c_str());
