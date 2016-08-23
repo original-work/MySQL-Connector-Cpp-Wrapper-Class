7d5bb830bd52aedb03c6e56c809180bb389436fd
winer632 <winer632@163.com>
2016-08-23 19:24:25 +0800

    test

--- main.cpp
+++ main.cpp
@@ -26,27 +26,42 @@ yum install -y boost boost-devel boost-doc
 #include <stdio.h>
 #include "mysqlconn_wrapper.h"
 
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+
+    return buf;
+}
 
 int main(int argc, char *argv[])
 {
 	MySQLConnWrapper db;
 	db.connect();
 	db.switchDb("mihao");
-	db.prepare("INSERT INTO active_user(mdn, imsi, esn) VALUES (?, ?, ?)");
-	db.setString(1,"13301602770");
-	db.setString(2, "6160761796");
-	db.setString(3, "80D8852E");
-	db.execute();
-	
-	db.setString(1,"13301605333");
-	db.setString(2, "0911785312");
-	db.setString(3, "0911785312");
+	db.prepare("INSERT INTO active_user(create_time, mdn, imsi, esn) VALUES (?, ?, ?, ?)");
+
+	string now=currentDateTime();
+	db.setString(1,now);
+	db.setString(2,"13301602770");
+	db.setString(3, "6160761796");
+	db.setString(4, "80D8852E");
 	db.execute();
 
-	
-	db.setString(1,"13301605371");
-	db.setString(2, "6301743459");
-	db.setString(3, "8094016C");
+	db.setString(1,now);
+	db.setString(2,"13301605333");
+	db.setString(3, "0911785312");
+	db.setString(4, "0911785312");
+	db.execute();
+
+	db.setString(1,now);
+	db.setString(2,"13301605371");
+	db.setString(3, "6301743459");
+	db.setString(4, "8094016C");
 	db.execute();
 
 
@@ -55,6 +70,8 @@ int main(int argc, char *argv[])
 	db.closePrepStmt();
 	
 	while(db.fetch()){
+		string create_time=db.getString("create_time");
+		printf("create_time=%s\n", create_time.c_str());
 		string mdn=db.getString("mdn");
 		printf("mdn=%s\n", mdn.c_str());
 		string imsi=db.getString("imsi");
